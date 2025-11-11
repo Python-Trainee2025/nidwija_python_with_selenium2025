8c8394bb82130126b1bbf51e44f233d74fc073cd
NidwijaB <nidwija.bhatta@cotiviti.com>
2025-11-11 13:50:50 +0000

    update

--- test_basic.py
+++ test_basic.py
@@ -3,13 +3,14 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-chrome_options=Options()
+def test_demo():
+    chrome_options=Options()
 
-driver=webdriver.Chrome(options=chrome_options)
-driver.maximize_window()
-driver.get("https://www.saucedemo.com/")
-time.sleep(3)
-print(driver.title)
-driver.quit()
+    driver=webdriver.Chrome(options=chrome_options)
+    driver.maximize_window()
+    driver.get("https://www.saucedemo.com/")
+    time.sleep(3)
+    print(driver.title)
+    driver.quit()
 
 
